a1d9d6d8f7d8f71ad36f5d2eb23dedcb0a7e973d
Daniel Khoury <daniel.khoury@heig-vd.ch>
2022-02-23 10:18:15 +0100

    fait bases des pages, ajouté du js, not working

--- Projet_vide/src/index.js
+++ Projet_vide/src/index.js
@@ -1,1 +1,49 @@
-import './css/index.css'
+import './css/index.css';
+// import domManipulator from "./js/domManipulator.js";
+
+function domOn(selector, event, callback) {
+    domForEach(selector, ele => ele.addEventListener(event, callback));
+  }
+  
+  // Param: selecteur + fonction a lancer
+  function domForEach(selector, callback) {
+    document.querySelectorAll(selector).forEach(callback);
+  }
+
+
+console.log("hello");
+const monBouton = document.querySelector('.leFooter');
+
+monBouton.addEventListener('click', evt => {
+  console.log(evt);
+});
+
+domOn('.leFooter', 'click', evt=>{
+    console.log("helloToYou")
+    console.log(evt);
+    console.log(evt.target)
+})
+
+domOn('body','click', evt=>{
+    console.log("hello Body")
+    console.log(evt);
+    console.log(evt.target)
+})
+
+domOn('#leBut', 'click', evt=>{
+    console.log("touch butt")
+})
+
+domOn('button', 'click', evt=>{
+  console.log("touch butt")
+})
+
+console.log(2+1)
+
+
+
+
+
+
+
+
---
+++ Projet_vide/src/js/domManipulator.js
@@ -0,0 +1,8 @@
+function domOn(selector, event, callback) {
+    domForEach(selector, ele => ele.addEventListener(event, callback));
+}
+
+// Param: selecteur + fonction a lancer
+function domForEach(selector, callback) {
+    document.querySelectorAll(selector).forEach(callback);
+}
