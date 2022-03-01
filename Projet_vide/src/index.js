de8a3939a8ea4a80802b09c16506d63770a5a856
Daniel Khoury <daniel.khoury@heig-vd.ch>
2022-03-01 14:13:12 +0100

    page fonctionnelle avec history git add index.html

--- Projet_vide/src/index.js
+++ Projet_vide/src/index.js
@@ -2,47 +2,34 @@ import './css/index.css';
 import domManipulator from "./js/domManipulator.js";
 
 function domOn(selector, event, callback) {
-    domForEach(selector, ele => ele.addEventListener(event, callback));
-  }
-  
-  // Param: selecteur + fonction a lancer
-  function domForEach(selector, callback) {
-    document.querySelectorAll(selector).forEach(callback);
-  }
+  domForEach(selector, ele => ele.addEventListener(event, callback));
+}
+
+// Param: selecteur + fonction a lancer
+function domForEach(selector, callback) {
+  document.querySelectorAll(selector).forEach(callback);
+}
 
 
-//  Selection du menu dynamique 
 
-domOn('.menu', 'click', evt=>{
-    console.log("helloToYou")
-    console.log(evt);
-    console.log(evt.target)
-    let selection = evt.target.id
-    console.log(selection);
-    domForEach('.section', evt=>{
-      //check ce qui est actif et le cache
-      if (evt.classList.contains('active')) {
-        window.setTimeout( function() {
-          evt.classList.remove('active');  
-      }, 100);
-      
-      evt.classList.add('hidden');   
+window.addEventListener('hashchange', evt => {
+  let hash = "#" + window.location.href.split('#')[1];
+  console.log(hash)
+  render(hash);
 
-      }
-      //affiche la séléction
-      if (evt.classList.contains(selection)) {
-        window.setTimeout( function() {
-          evt.classList.add('active');
-          evt.classList.remove('hidden');
-      }, 100);
-      
-
-        
-      }
-    })
-    
 })
 
+function render(hash) {
+  domForEach('.section', elm=>{
+    if(elm.classList.contains(hash.substring(1))){
+      elm.classList.remove('hidden')
+      elm.classList.add("active")
+    } else{
+      elm.classList.remove('active')
+      elm.classList.add('hidden')
+    }
+  })
+}
 
 
 
