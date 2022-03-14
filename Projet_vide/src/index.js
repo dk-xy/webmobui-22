1c61f2a2376bc727be6206771a19b91dd31864fc
dk-xy <daniel.khoury@heig-vd.ch>
2022-03-14 20:01:38 +0100

    modifié la liste de chanson, est maintenant fonctionnelle

--- Projet_vide/src/index.js
+++ Projet_vide/src/index.js
@@ -16,61 +16,137 @@ function domForEach(selector, callback) {
 
 window.addEventListener('hashchange', evt => {
   let hash = "#" + window.location.href.split('#')[1];
-  console.log(hash) 
+  console.log(hash)
   render(hash);
 
 })
 
-window.addEventListener('load', evt=>{
+window.addEventListener('load', evt => {
   let hash = "#" + window.location.href.split('#')[1];
-  console.log(hash) 
-  render(hash); 
+  //console.log(hash)
+  render(hash);
+  
 })
 
+
+
 function render(hash) {
-  domForEach('.section', elm=>{
+
+  
+
+
+  domForEach('.section', elm => {
     console.log(hash)
-    if(elm.classList.contains(hash.substring(1))){
+    console.log(hash.substring(1))
+    if (elm.classList.contains(hash.substring(1))) {
       elm.classList.remove('hidden')
       elm.classList.add("active")
-    } else{
+    } else {
       elm.classList.remove('active')
       elm.classList.add('hidden')
     }
-  })
-  if (hash == "#artist") {
-    getArtists()
-  }
-}
-
-
-
-async function getArtists(){
-  let link ="https://webmob-ui-22-spotlified.herokuapp.com/api/artists";
-  const TMPL_ARTI = document.querySelector('.artistInd');
-  console.log(TMPL_ARTI)
-
-  fetch(link)
-  .then((resp) => resp.json())
-  .then((artistes) =>{
-    console.log(artistes)
-
-    //console.log(artistes)
-    artistes.forEach(elm => {
-      let tmpl = TMPL_ARTI.cloneNode(true)
-      tmpl.classList.remove('hidden');
-      console.log(elm.name)
-      tmpl.querySelector('img').setAttribute('src', elm.image_url)
-      tmpl.querySelector('.artistName').textContent = elm.name;
-      // tmpl.querySelector('a').setAttribute = ('href', "#artists-"+elm.id)
-      document.querySelector('#artiste').appendChild(tmpl)
-
-    });
     
   })
 
+  if (hash == "#artist") {
+    getArtists()
+  } else if (hash.split('-').length > 1) {
+    //console.log(hash);
+    console.log(hash)
+    let tabLink = hash.split('-')
+    let artistID = tabLink[1]
+    getSongs(artistID);
+    document.querySelector('.artistSongs').classList.remove('hidden')
+  }
 
 }
+
+// async function getSongs(){
+//   domOn('.artistInd', 'click', evt=>{
+//     console.log(evt.target.parentElement)
+//     let target = evt.target.parentElement;
+//     console.log(target.getAttribute('artistno')) 
+//   })
+
+// }
+
+
+
+
+
+
+//PARTIE MUSIQUE -----------------------------------------------------------------------
+async function getSongs(id) {
+  let link = "https://webmob-ui-22-spotlified.herokuapp.com/api/artists/" + id + "/songs"
+ let listNode = document.querySelector('.listeTitres');
+ if(listNode.hasChildNodes()){
+  listNode.removeChild(listNode.firstChild);
+ }
+
+  fetch(link)
+    .then((resp) => resp.json())
+    .then((songs) => {
+      console.log(songs)
+
+        document.querySelector('.listeTitres').append(document.createElement('ul'))
+
+      
+      songs.forEach(song => {
+        let lit = document.querySelector('.listeTitres ul').appendChild(document.createElement('li'))
+        // let elmList = document.querySelector('.listeTitres ul')
+        // elmList.append(document.createElement('li'))
+        // document.querySelector('.listeTitres ul li').append(song.title)
+        lit.append(song.title)
+
+      });
+
+
+    }
+    )
+}
+
+//PARTIE ARTISTE-------------------------------------------------------------
+let artistIsLoaded = false;
+
+async function getArtists() {
+  let link = "https://webmob-ui-22-spotlified.herokuapp.com/api/artists";
+  fetch(link)
+    .then((resp) => resp.json())
+    .then((artistes) => {
+      console.log(artistes)
+      console.log(artistIsLoaded)
+
+      //Chargement artistes---------------------------
+      if (!artistIsLoaded) {
+        artistes.forEach(elm => {
+          makeArtists(elm)
+          // tmpl.querySelector('a').setAttribute = ('href', "#artists-"+elm.id)
+
+          // tmpl.querySelector('.artistInd').setAttribute('id', '#artistInd-'+elm.id)
+          artistIsLoaded = true;
+          return artistIsLoaded;
+        });
+        //Chargement chansons------------------------------
+
+      }
+
+    })
+}
+
+async function makeArtists(art) {
+  const TMPL_ARTI = document.querySelector('.artistInd');
+  let tmpl = TMPL_ARTI.cloneNode(true)
+  tmpl.id = "#artistInd-" + art.id
+  tmpl.setAttribute('artistno', art.id)
+  tmpl.setAttribute('href', '#artist-' + art.id)
+  tmpl.classList.remove('hidden');
+  tmpl.querySelector('img').setAttribute('src', art.image_url)
+  tmpl.querySelector('.artistName').textContent = art.name;
+  document.querySelector('#artiste').appendChild(tmpl)
+
+}
+
+
 
 
 
